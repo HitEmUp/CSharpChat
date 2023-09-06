2ad5cb91fc7bcd079992c7cefa320f1f22f1c455
HitEmUp <samanrazavi@web.de>
2023-09-06 18:00:14 +0200

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbxLocalIP = new System.Windows.Forms.TextBox();
+            this.tbxLocalPort = new System.Windows.Forms.TextBox();
+            this.lbxChat = new System.Windows.Forms.ListBox();
+            this.tbxChatToSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxRemotePort = new System.Windows.Forms.TextBox();
+            this.tbxRemoteIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(233, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 51);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbxLocalIP
+            // 
+            this.tbxLocalIP.Location = new System.Drawing.Point(78, 14);
+            this.tbxLocalIP.Name = "tbxLocalIP";
+            this.tbxLocalIP.Size = new System.Drawing.Size(96, 20);
+            this.tbxLocalIP.TabIndex = 1;
+            this.tbxLocalIP.Text = "127.0.0.1";
+            // 
+            // tbxLocalPort
+            // 
+            this.tbxLocalPort.Location = new System.Drawing.Point(180, 14);
+            this.tbxLocalPort.Name = "tbxLocalPort";
+            this.tbxLocalPort.Size = new System.Drawing.Size(44, 20);
+            this.tbxLocalPort.TabIndex = 2;
+            this.tbxLocalPort.Text = "27000";
+            // 
+            // lbxChat
+            // 
+            this.lbxChat.FormattingEnabled = true;
+            this.lbxChat.Location = new System.Drawing.Point(15, 89);
+            this.lbxChat.Name = "lbxChat";
+            this.lbxChat.Size = new System.Drawing.Size(430, 160);
+            this.lbxChat.TabIndex = 4;
+            // 
+            // tbxChatToSend
+            // 
+            this.tbxChatToSend.Location = new System.Drawing.Point(15, 257);
+            this.tbxChatToSend.Name = "tbxChatToSend";
+            this.tbxChatToSend.Size = new System.Drawing.Size(349, 20);
+            this.tbxChatToSend.TabIndex = 5;
+            this.tbxChatToSend.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbxChatToSend_KeyUp);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(370, 255);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Remote IP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Your IP:";
+            // 
+            // tbxRemotePort
+            // 
+            this.tbxRemotePort.Location = new System.Drawing.Point(180, 43);
+            this.tbxRemotePort.Name = "tbxRemotePort";
+            this.tbxRemotePort.Size = new System.Drawing.Size(44, 20);
+            this.tbxRemotePort.TabIndex = 14;
+            this.tbxRemotePort.Text = "27000";
+            // 
+            // tbxRemoteIP
+            // 
+            this.tbxRemoteIP.Location = new System.Drawing.Point(78, 43);
+            this.tbxRemoteIP.Name = "tbxRemoteIP";
+            this.tbxRemoteIP.Size = new System.Drawing.Size(96, 20);
+            this.tbxRemoteIP.TabIndex = 13;
+            this.tbxRemoteIP.Text = "127.0.0.1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 298);
+            this.Controls.Add(this.tbxRemotePort);
+            this.Controls.Add(this.tbxRemoteIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbxChatToSend);
+            this.Controls.Add(this.lbxChat);
+            this.Controls.Add(this.tbxLocalPort);
+            this.Controls.Add(this.tbxLocalIP);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbxLocalIP;
+        private System.Windows.Forms.TextBox tbxLocalPort;
+        private System.Windows.Forms.ListBox lbxChat;
+        private System.Windows.Forms.TextBox tbxChatToSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxRemotePort;
+        private System.Windows.Forms.TextBox tbxRemoteIP;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chat
+{
+    public partial class Form1 : Form
+    {
+        // Attribute
+        private Socket socket;
+        private IPEndPoint localIP;
+        private EndPoint remoteIP;
+        private byte[] buffer;
+        private IAsyncResult aResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set up socket
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            // get own IP
+            tbxLocalIP.Text = GetLocalIP();
+            tbxRemoteIP.Text = GetLocalIP();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // binding socket
+                localIP = new IPEndPoint(IPAddress.Parse(tbxLocalIP.Text), Convert.ToInt32(tbxLocalPort.Text));
+                socket.Bind(localIP);
+                // connect to remote IP and port
+                remoteIP = new IPEndPoint(IPAddress.Parse(tbxRemoteIP.Text),
+                Convert.ToInt32(tbxRemotePort.Text));
+                socket.Connect(remoteIP);
+                // starts to listen to an specific port
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref remoteIP, new AsyncCallback(ReciveMessageCallBack), buffer);
+                
+                // release button to send message
+                btnSend.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ReciveMessageCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                aResult = ar;
+                int size = socket.EndReceiveFrom(aResult, ref remoteIP);
+                
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1500];
+                    receivedData = (byte[])aResult.AsyncState;
+                
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+                    lbxChat.Items.Add("Remote: " + receivedMessage);
+                }
+                // starts to listen the socket again
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref remoteIP, new AsyncCallback(ReciveMessageCallBack), buffer);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(tbxChatToSend.Text);
+                socket.Send(msg);
+                
+                lbxChat.Items.Add("Local: " + tbxChatToSend.Text);
+                tbxChatToSend.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+
+        private void tbxChatToSend_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSend_Click(sender, e);
+            }
+        }
+    }
+}
